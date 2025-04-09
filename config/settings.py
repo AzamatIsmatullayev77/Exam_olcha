0a3d8f85e721fa4396e227d7a076dd24da4f624c
AzamatIsmatullayev77 <azamathojawood@gmail.com>
2025-04-09 23:50:33 +0500

    commit

--- config/settings.py
+++ config/settings.py
@@ -111,6 +111,11 @@ USE_TZ = True
 
 # Static and Media files
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'olcha/media/')
 
